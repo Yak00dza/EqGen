c36ea7d005506e2ce557ba96683889e50e3c9dbf
Yakiv <yakiv.baiduk@novoshkola.com.ua>
2023-06-26 23:15:11 +0300

    Fixed previous commit (wrong .gitignore configuration). Added properties folder in Generators

---
+++ Generators/Properties/ConstantGeneratorProperties.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EqGen.Generators.Properties
+{
+    class ConstantGeneratorProperties
+    {
+        private int minValue;
+        private int maxValue;
+        private bool isOutFloat;
+
+        public ConstantGeneratorProperties(int minValue, int maxValue, bool isOutFloat)
+        {
+            this.minValue = minValue;
+            this.maxValue = maxValue;
+            this.isOutFloat = isOutFloat;
+
+        }
+        public bool IsOutFloat()
+        {
+            return isOutFloat;
+        }
+        public int GetMinValue()
+        {
+            return minValue;
+        }
+        public int GetMaxValue()
+        {
+            return maxValue;
+        }
+    }
+}
